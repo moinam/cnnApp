034b2f8391a6dce544c94a0b76eb4748cfa8b83d
Moinam Chatterjee <moinamc2007@yahoo.co.in>
2022-09-09 20:01:32 +0200

    remove unnecessary files

--- frontend/ms-repo/src/app/core/table-filter.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-
-import { TableFilterPipe } from './table-filter.pipe';
-
-@NgModule({
-  declarations: [TableFilterPipe],
-  exports: [TableFilterPipe],
-})
-export class TableFilterModule {}
