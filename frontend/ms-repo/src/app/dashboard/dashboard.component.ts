09315639897d4fef78fc9f75fbe2e8cd0836a191
Moinam Chatterjee <moinamc2007@yahoo.co.in>
2022-09-09 20:03:05 +0200

    update dashboard and support

--- frontend/ms-repo/src/app/dashboard/dashboard.component.ts
+++ frontend/ms-repo/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../core/data.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,87 +9,31 @@ import { NgxSpinnerService } from 'ngx-spinner';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  selectedClient: any;
-  selectedTaskType: any;
-  quantity: any;
-  search: any = '';
-  teams: any;
   filterCriteria: any;
-  dispMap: boolean = false;
-  currentClient: any;
-  clientLocation: any;
-  teamLocation: any;
-  map_url = '';
+  uploadedFile: any;
   constructor(
     public dataService: DataService,
-    private spinner: NgxSpinnerService
-  ) {
-    this.filterCriteria = {
-      clientId: '',
-      taskType: '',
-      quantity: '',
-    };
-
-    this.dataService.locationReady.subscribe((load: boolean) => {
-      if (load) {
-        console.log(this.dataService.clientLocation);
-        console.log(this.dataService.teamLocation);
-        this.map_url =
-          'https://maps.googleapis.com/maps/api/staticmap?size=650x350&maptype=roadmap&markers=color:blue%7Clabel:C%7C' +
-          this.dataService.clientLocation.lat +
-          ',' +
-          this.dataService.clientLocation.lng +
-          '&markers=color:green%7Clabel:T%7C' +
-          this.dataService.teamLocation.lat +
-          ',' +
-          this.dataService.teamLocation.lng +
-          '&key=' +
-          this.dataService.map_config.key;
-        this.dispMap = true;
-        this.spinner.hide();
-        this.dataService.locationReady.next(false);
-      }
-    });
-  }
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {}
 
-  getDate(timestamp: any): any {
-    let hours = Math.floor(timestamp / 60 / 60);
-    let minutes = Math.floor(timestamp / 60) - hours * 60;
-    let formatted = '';
-    if (hours > 0) {
-      formatted += hours.toString() + ' Hrs';
-      if (minutes > 0){
-        formatted += ' and ';
-      }
-    }
-    if (minutes > 0) {
-      formatted += minutes.toString() + ' min ';
-    }
-    return formatted;
+  onUpload(event: any) {
+    this.uploadedFile = event.files[0];
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Success',
+      detail: 'File Uploaded',
+    });
+    console.log(this.uploadedFile);
   }
 
-  getPrediction(): any {
-    this.currentClient = this.selectedClient;
-    this.filterCriteria.clientId = this.selectedClient.clientId;
-    this.filterCriteria.taskType = this.selectedTaskType;
-    this.filterCriteria.quantity = this.quantity;
-    console.log(this.filterCriteria);
-    console.log('Hit!!');
-    this.dataService.getTeams(this.filterCriteria);
-  }
-  showMap(team: any) {
-    this.spinner.show();
-    this.clientLocation = this.dataService.fetchGeoLocation(
-      this.currentClient.location,
-      team.location
-    );
+  reset() {
+    this.uploadedFile = null;
+    this.dataService.resAvail = false;
   }
 
-  reset(): any {
-    this.selectedClient = null;
-    this.selectedTaskType = null;
-    this.quantity = null;
+  getPrediction() {
+    this.dataService.getRecognition(this.uploadedFile);
   }
 }
